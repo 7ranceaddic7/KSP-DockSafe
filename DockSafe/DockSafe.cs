cb95e7f205d5b1f98f9e3370f64c985a912eac21
severedsolo <severedsolo@users.noreply.github.com>
2020-02-19 09:49:38 +0000

    Update for 1.9,  engines reactivate automatically

--- DockSafe/DockSafe.cs
+++ DockSafe/DockSafe.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using KSP.UI.Screens;
 
@@ -7,38 +7,41 @@ namespace DockSafe
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class DockSafe : MonoBehaviour
 	{
-		private ApplicationLauncherButton AppLauncherButton;
+		private ApplicationLauncherButton appLauncherButton;
 		private bool isActive;
+		private readonly List<ModuleEngines> deactivatedEngines = new List<ModuleEngines>();
 
-		void Start()
+		private void Start()
 		{
 			isActive = false;
-			GameEvents.onGUIApplicationLauncherReady.Add (OnAppLauncherReady);
-			GameEvents.onVesselChange.Add (OnVesselChange);
+			GameEvents.onGUIApplicationLauncherReady.Add(OnAppLauncherReady);
+			GameEvents.onVesselChange.Add(OnVesselChange);
 		}
 
-		void Update()
+		private void Update()
 		{
-			if (!isActive) {
-				if (FlightGlobals.ActiveVessel.targetObject is ModuleDockingNode) {
-					AppLauncherButton.SetTrue ();
-				}
+			if (FlightGlobals.ActiveVessel.targetObject is ModuleDockingNode)
+			{
+				appLauncherButton.SetTrue();
 			}
+			else if(isActive) appLauncherButton.SetFalse();
 		}
 
-		void OnDestroy()
+		private void OnDestroy()
 		{
 			GameEvents.onGUIApplicationLauncherReady.Remove(OnAppLauncherReady);
-			GameEvents.onVesselChange.Remove (OnVesselChange);
-			if (AppLauncherButton != null) {
-				ApplicationLauncher.Instance.RemoveModApplication (AppLauncherButton);
+			GameEvents.onVesselChange.Remove(OnVesselChange);
+			if (appLauncherButton != null)
+			{
+				ApplicationLauncher.Instance.RemoveModApplication(appLauncherButton);
 			}
 		}
 
 		public void OnAppLauncherReady()
 		{
-			if (AppLauncherButton == null) {
-				AppLauncherButton = ApplicationLauncher.Instance.AddModApplication (
+			if (appLauncherButton == null)
+			{
+				appLauncherButton = ApplicationLauncher.Instance.AddModApplication(
 					Activate,
 					Deactivate,
 					null,
@@ -46,87 +49,107 @@ namespace DockSafe
 					null,
 					null,
 					ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW,
-					GameDatabase.Instance.GetTexture ("DockSafe/Textures/ds_off", false)
+					GameDatabase.Instance.GetTexture("DockSafe/Textures/ds_off", false)
 				);
 			}
-			// If vessel has disabled engines, then DS was active previously, reinitiate override
-			if (CheckActive ()) {
-				AppLauncherButton.SetTrue ();
+
+			// If vessel has disabled engines, then DS was active previously, re-activate override
+			if (CheckActive())
+			{
+				appLauncherButton.SetTrue();
 			}
 		}
 
 		// Activate app
-		private void Activate() {
-			if (!isActive) {
-				isActive = true;
-				ShutdownEngines ();
-				AppLauncherButton.SetTexture (
-					GameDatabase.Instance.GetTexture("DockSafe/Textures/ds_active", false)
-				);
-				ScreenMessages.PostScreenMessage ("DockSafe: Engines safety override active");
-			}
+		private void Activate()
+		{
+			if (isActive) return;
+			isActive = true;
+			ShutdownEngines();
+			appLauncherButton.SetTexture(
+				GameDatabase.Instance.GetTexture("DockSafe/Textures/ds_active", false)
+			);
+			ScreenMessages.PostScreenMessage("DockSafe: Engines safety override active");
 		}
 
 		// Deactivate app
-		private void Deactivate() {
+		private void Deactivate()
+		{
 			isActive = false;
-			ActivateEngines ();
-			ScreenMessages.PostScreenMessage ("DockSafe: Deactivated");
-			AppLauncherButton.SetTexture (
+			ActivateEngines();
+			ScreenMessages.PostScreenMessage("DockSafe: Deactivated");
+			appLauncherButton.SetTexture(
 				GameDatabase.Instance.GetTexture("DockSafe/Textures/ds_off", false)
 			);
 		}
 
 		// Shutdown engines and override control
-		private void ShutdownEngines() {
-			foreach (Part p in FlightGlobals.ActiveVessel.Parts) {
-				foreach(PartModule pm in p.Modules) {
-					if (pm is ModuleEngines) {
-						ModuleEngines eng = (ModuleEngines)pm;
-						if (eng.engineType == EngineType.LiquidFuel | eng.engineType == EngineType.Nuclear) {
-							eng.Shutdown ();
-							eng.Events ["Activate"].guiActive = false;
-						}
-					}
+		private void ShutdownEngines()
+		{
+			foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+			{
+				foreach (PartModule pm in p.Modules)
+				{
+					if (!(pm is ModuleEngines)) continue;
+					ModuleEngines eng = (ModuleEngines) pm;
+					if (!(eng.engineType == EngineType.LiquidFuel | eng.engineType == EngineType.Nuclear)) continue;
+					if(!eng.Events["Activate"].active)deactivatedEngines.Add(eng);
+					eng.Shutdown();
+					eng.Events["Activate"].guiActive = false;
 				}
 			}
 		}
 
 		// Restore engines control without reactivating them
-		private void ActivateEngines() {
-			foreach (Part p in FlightGlobals.ActiveVessel.Parts) {
-				foreach(PartModule pm in p.Modules) {
-					if (pm is ModuleEngines) {
-						ModuleEngines eng = (ModuleEngines)pm;
-						if (eng.engineType == EngineType.LiquidFuel | eng.engineType == EngineType.Nuclear) {
-							eng.Events ["Activate"].guiActive = true;
-						}
+		private void ActivateEngines()
+		{
+			foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+			{
+				foreach (PartModule pm in p.Modules)
+				{
+					if (!(pm is ModuleEngines)) continue;
+					ModuleEngines eng = (ModuleEngines) pm;
+					if (eng.engineType == EngineType.LiquidFuel | eng.engineType == EngineType.Nuclear)
+					{
+						eng.Events["Activate"].guiActive = true;
 					}
+
+					if (!deactivatedEngines.Contains(eng)) continue;
+					eng.Activate();
+					deactivatedEngines.Remove(eng);
 				}
 			}
 		}
 
 		// Check if there are disabled engines
-		private bool CheckActive() {
-			foreach (Part p in FlightGlobals.ActiveVessel.Parts) {
-				foreach(PartModule pm in p.Modules) {
-					if (pm is ModuleEngines) {
-						ModuleEngines eng = (ModuleEngines)pm;
-						if (eng.Events ["Activate"].guiActive == false) {
-							return true;
-						}
+		private bool CheckActive()
+		{
+			foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+			{
+				foreach (PartModule pm in p.Modules)
+				{
+					if (!(pm is ModuleEngines)) continue;
+					ModuleEngines eng = (ModuleEngines) pm;
+					if (eng.Events["Activate"].guiActive == false)
+					{
+						return true;
 					}
 				}
 			}
+
 			return false;
 		}
 
-		// If vessel has disabled engines, then DS was active previously, reinitiate override
-		private void OnVesselChange (Vessel v) {
-			if (CheckActive ()) {
-				AppLauncherButton.SetTrue ();
-			} else {
-				AppLauncherButton.SetFalse ();
+		// If vessel has disabled engines, then DS was active previously, re-activate override
+		private void OnVesselChange(Vessel v)
+		{
+			if (CheckActive())
+			{
+				appLauncherButton.SetTrue();
+			}
+			else
+			{
+				appLauncherButton.SetFalse();
 			}
 		}
 	}
